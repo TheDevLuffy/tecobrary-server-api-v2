95bfc2b9210df90b0f68745d62f8a080d620265f
thedevluffy <thedevluffy@gmail.com>
2019-12-12 16:31:19 +0900

    [#43] refactor: 불필요한 yml utility 를 제거한다.

--- src/main/java/com/woowacourse/tecobrary/TecobraryApplication.java
+++ src/main/java/com/woowacourse/tecobrary/TecobraryApplication.java
@@ -1,24 +1,11 @@
 package com.woowacourse.tecobrary;
 
-import com.woowacourse.tecobrary.utility.YamlPropertySourceFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource(
-        value = {
-                "classpath:github.yml",
-                "classpath:jwtauth.yml"
-        },
-        ignoreResourceNotFound = true,
-        factory = YamlPropertySourceFactory.class
-)
-
 public class TecobraryApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TecobraryApplication.class, args);
     }
-
 }
